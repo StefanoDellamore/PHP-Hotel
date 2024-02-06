c98d29afb6a69ce03d51b580f2d71c05454b23db
StefanDellamore <dellamoreste.info@gmail.com>
2024-02-06 16:26:46 +0100

    Stampa dell'array

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,6 +50,36 @@
         <title>PHP Hotel</title>
     </head>
     <body>
+        
+        <div>
+            <?php 
+                foreach ($hotels as $hotel) { ?>
+                <h2>
+                    <?php echo $hotel ['name']?>
+                </h2>
+
+                <p>
+                    Descrizione: <?php echo $hotel ['description']?>
+                </p>
+
+                <p>
+                    Parking: <?php echo $hotel ['parking'] ? 'Yes' : 'No' ?>
+                </p>
+
+                <p>
+                    Vote: <?php echo $hotel ['vote']?>
+                </p>
+
+                <p>
+                    Distance to centre: <?php echo $hotel['distance_to_center'] ?>
+                </p>
+
+            
+        </div>
+        <?php } ?>
+        
+        
+        
         
     </body>
 </html>
